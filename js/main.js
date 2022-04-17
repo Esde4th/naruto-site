fe2900966a62c55f95da2678f905831d6900e339
Bambietta <bambi666@example.com>
2022-04-17 22:56:22 +0200

    gotovo

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: false,
+        dots: false,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+        easing: 'linear',
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        draggable: false,
+        pauseOnFocus: false,
+        touchMove: true,
+        waitForAnimate: true,
+        centerMode: true,
+        variableWidth: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+        mobileFirst: false,
+    });
+});
